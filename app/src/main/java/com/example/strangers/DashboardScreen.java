73a717366900dbcd2ceaf40a2a65cc4c8ea286d9
dineshkushwah5252@gmail.com <dineshkushwah5252@gmail.com>
2022-06-04 11:11:13 +0530

    first commit

--- app/src/main/java/com/example/strangers/DashboardScreen.java
+++ app/src/main/java/com/example/strangers/DashboardScreen.java
@@ -1,12 +1,14 @@
 package com.example.strangers;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
 import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
 
 import java.net.MalformedURLException;
@@ -28,29 +30,39 @@ public class DashboardScreen extends AppCompatActivity {
         secretCodeBox = findViewById(R.id.secretBox);
         joinnow = findViewById(R.id.joinnow);
         sharenow = findViewById(R.id.sharenow);
-
-        URL serverURL = null;
-
+        URL serverURL;
         try {
-            new URL("https://meet.jit.si");
-            JitsiMeetConferenceOptions defaultOptions;
-            defaultOptions = new JitsiMeetConferenceOptions.Builder()
-                    .setServerURL(serverURL)
-                    .setWelcomePageEnabled(false)
-                    .build();
-            JitsiMeet.setDefaultConferenceOptions(defaultOptions);
-
+            // When using JaaS, replace "https://meet.jit.si" with the proper serverURL
+            serverURL = new URL("https://meet.jit.si");
         } catch (MalformedURLException e) {
             e.printStackTrace();
+            throw new RuntimeException("Invalid server URL!");
         }
+        JitsiMeetConferenceOptions defaultOptions
+                = new JitsiMeetConferenceOptions.Builder()
+                .setServerURL(serverURL)
+//                 When using JaaS, set the obtained JWT here
+                .setToken("MyJWT")
+//                 Different features flags can be set
+                 .setFeatureFlag("toolbox.enabled", false)
+                 .setFeatureFlag("filmstrip.enabled", false)
+                .setFeatureFlag("welcomepage.enabled", false)
+                .build();
+        JitsiMeet.setDefaultConferenceOptions(defaultOptions);
 
-        
 
 
-        joinnow.setOnClickListener(v -> {
+        joinnow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                        .setRoom(secretCodeBox.getText().toString())
 
+                        .build();
+
+                JitsiMeetActivity.launch(DashboardScreen.this, options);
+            }
         });
-
 
     }
 }
